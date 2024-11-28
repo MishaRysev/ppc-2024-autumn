548c9a0c8f1ef1e959afba7fa8e44111b1a94e99
MishaRysev <r0botbob@yandex.ru>
2024-11-28 17:59:21 +0300

    Рысев Михаил. Задача 2. Вариант 14. Гиперкуб

---
+++ tasks/mpi/rysev_m_gypercube/func_tests/main.cpp
@@ -0,0 +1,50 @@
+// Copyright 2023 Nesterov Alexander
+#include <gtest/gtest.h>
+
+#include <boost/mpi/communicator.hpp>
+#include <boost/mpi/environment.hpp>
+#include <vector>
+
+#include "mpi/rysev_m_gypercube/include/ops_mpi.hpp"
+
+TEST(rysev_m_gypercube, data_transfer) {
+  boost::mpi::communicator world;
+
+  std::vector<uint8_t> in(10);
+  std::vector<uint8_t> out(10);
+  for (uint8_t i = 0; i < in.size(); i++) { in[i] = i; }
+
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+  taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  taskDataPar->inputs_count.emplace_back(in.size());
+  taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataPar->outputs_count.emplace_back(out.size());
+  
+  rysev_m_gypercube::GyperCube task(taskDataPar);
+  ASSERT_TRUE(task.validation());
+  task.pre_processing();
+  ASSERT_TRUE(task.run());
+  task.post_processing();
+  
+  for (uint8_t i = 0; i < out.size(); i++) { ASSERT_EQ(out[i], in[i]); }
+}
+
+TEST(t_rysev_m_gypercube, more_than_expected_has_arrived){
+  boost::mpi::communicator world;
+
+  std::vector<uint8_t> in(15);
+  std::vector<uint8_t> out(10);
+  for (int i = 0; i < in.size(); i++) { in[i] = i; }
+
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+  taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  taskDataPar->inputs_count.emplace_back(in.size());
+  taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  taskDataPar->outputs_count.emplace_back(out.size());
+  
+  rysev_m_gypercube::GyperCube task(taskDataPar);
+  ASSERT_TRUE(task.validation());
+  task.pre_processing();
+  ASSERT_FALSE(task.run());
+  task.post_processing();
+}
---
+++ tasks/mpi/rysev_m_gypercube/src/ops_mpi.cpp
@@ -0,0 +1,47 @@
+// Copyright 2023 Nesterov Alexander
+
+#include "mpi/rysev_m_gypercube/include/ops_mpi.hpp"
+
+#include <algorithm>
+#include <functional>
+#include <random>
+#include <string>
+#include <thread>
+#include <vector>
+using namespace std::chrono_literals;
+
+bool rysev_m_gypercube::GyperCube::pre_processing() {
+  internal_order_test();
+  return true;
+}
+
+bool rysev_m_gypercube::GyperCube::validation() {
+  internal_order_test();
+  if (world.size() < 2 || taskData->inputs.empty()) return false;
+  return true;
+}
+
+bool rysev_m_gypercube::GyperCube::run() {
+  internal_order_test();
+  for (int i = 0; i < std::log2(world.size()); i++){	  
+    int neighbr = world.rank() ^ (1 << i);
+	if (neighbr < world.size()) {
+	  std::vector<uint8_t> to_send(taskData->inputs_count[0]), to_recv(taskData->inputs_count[0]);
+	  std::copy(taskData->inputs[0], taskData->inputs[0] + taskData->inputs_count[0], to_send.begin());
+	
+	  world.send(neighbr, 0, to_send);
+	  world.recv(neighbr, 0, to_recv);
+
+	  if (taskData->outputs_count[0] >= to_recv.size()) { std::copy(to_recv.begin(), to_recv.begin() + to_recv.size(), taskData->outputs[0]); }
+      else { return false; }
+	}
+	world.barrier();
+  }
+  
+  return true;
+}
+
+bool rysev_m_gypercube::GyperCube::post_processing() {
+  internal_order_test();
+  return true;
+}
