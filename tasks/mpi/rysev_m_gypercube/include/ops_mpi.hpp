02a44bd0b40edd3ae22abddcc491e912a3b35c85
MishaRysev <r0botbob@yandex.ru>
2024-11-28 17:59:21 +0300

    Рысев Михаил. Задача 2. Вариант 14. Гиперкуб

---
+++ tasks/mpi/rysev_m_gypercube/include/ops_mpi.hpp
@@ -0,0 +1,30 @@
+// Copyright 2023 Nesterov Alexander
+#pragma once
+
+#include <gtest/gtest.h>
+
+#include <boost/mpi/collectives.hpp>
+#include <boost/mpi/communicator.hpp>
+#include <memory>
+#include <numeric>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+namespace rysev_m_gypercube {
+
+class GyperCube : public ppc::core::Task {
+ public:
+  explicit GyperCube(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  boost::mpi::communicator world;
+};
+
+}  // rysev_m_gypercube
